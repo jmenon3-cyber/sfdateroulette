406bb20585d2b4759c9dc6623dbd62dc4c72af6e
Jay Menon <jayvmenon3@gmail.com>
2026-01-28 23:20:23 -0800

    added browser icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Component {...pageProps} />
       <div className="app-footer">@jmenon3</div>
       <Analytics />
